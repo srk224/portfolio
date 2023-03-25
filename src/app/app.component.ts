bc64c72193680aa5758b46cc866f1a2141ff973a
KimberleyChong <kim.barleyyy@gmail.com>
2023-03-25 16:24:39 -0400

    add scroll progress bar, update footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  ngOnInit(): void {
+    window.onscroll = (event) => this.scrollProgress();
+  }
+
+  scrollProgress() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("bar")!.style.width = scrolled + "%";
+  }   
+  
 }
