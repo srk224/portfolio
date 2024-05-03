7a9ee5bd96e08dcfb382a39044ea5d3de4d4a75d
unknown <rocklion224@gmail.com>
2024-05-03 17:58:36 -0400

    github pages

--- src/about.js
+++ src/about.js
@@ -6,7 +6,7 @@ function About() {
   return (
     <div className="about">
           <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
-              <h1 class="about_heading">ABOUT US </h1> <br />
+              <h1 class="about_heading">ABOUT ME</h1> <br />
           </ScrollAnimation>
               <h3 class="about_me">Computational Mathematics Student at the University of Waterloo</h3>
     </div>
