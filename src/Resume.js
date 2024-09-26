3b45078b19e42b5492e0f1b5add08c6d134895a4
Shahrukh Khan <abdulshahrukhkhan@Shahrukhs-MacBook-Pro.local>
2024-09-25 22:45:14 -0400

    Resume updated

--- src/Resume.js
+++ src/Resume.js
@@ -7,7 +7,7 @@ function Resume(){
     className = "resume_link"
     target="_blank" 
     rel="noreferrer noopener" 
-    href = "https://drive.google.com/file/d/1LetHPdrcs3cELG62S-5Oi647lsb6ZKuZ/view?usp=sharing"
+    href = "https://drive.google.com/file/d/1gl1bzU4ghHKE9IpI-ptMq2Cjxl7mjd-b/view?usp=sharing"
     whileHover={{ scale: 1.1 , rotate: 360}}
     whileTap={{ scale: 0.9 }}>
     
